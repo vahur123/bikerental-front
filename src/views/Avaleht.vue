84939834d0235978ed79ff314c89abf3f2379c55
Aleksei Gornev <vigadeparandus@gmail.com>
2022-02-22 15:07:35 +0200

    ALEKSEI-TASK1 added showBikeModelsByType

--- src/views/Avaleht.vue
+++ src/views/Avaleht.vue
@@ -1,16 +1,29 @@
 <template>
-<div>
-  <h1>Tere tulemast Vali-Rattarenti!</h1>
-  <br>
-  <h3>Palun vali, millist tüüpi ratast soovid rentida:</h3>
   <div>
-    <select v-model="selected">
-      <option disabled value="">Vali rattatüüp</option>
-      <option v-for="option in options" :value="option.id">{{option.name}}</option>
-    </select>
+    <h1>Tere tulemast Vali-Rattarenti!</h1>
+    <br>
+    <h3>Palun vali, millist tüüpi ratast soovid rentida:</h3>
+    <div>
+      <select v-on:change="showBikeModelByType" v-model="selected">
+        <option disabled value="">Vali rattatüüp</option>
+        <option v-for="option in options" :value="option.id">{{ option.name }}</option>
+      </select>
+    </div>
+<!--    <span>typeId: {{ selected }}</span>-->
+    <br><br>
+<!--    <input placeholder="rattatüüp" v-model="type">-->
+<!--    <button v-on:click="showBikeModelByType">Lae rattad</button>-->
+    <table v-if="selected">
+      <tr>
+        <th>Rattamudel</th>
+        <th>Raami suurus</th>
+      </tr>
+      <tr v-for="row in models">
+        <td>{{ row.bikeName }}</td>
+        <td>{{ row.sizeName }}</td>
+      </tr>
+    </table>
   </div>
-
-</div>
 </template>
 
 <script>
@@ -19,6 +32,8 @@ export default {
   data: function () {
     return {
       options: [],
+      type: '',
+      models: [],
       selected: ""
     }
   },
@@ -31,13 +46,25 @@ export default {
           }).catch(error => {
         console.log(error)
       })
+    },
+    showBikeModelByType: function () {
+      this.$http.get("/bike/model/by/type", {
+        params: {
+          typeId: this.selected
+        }
+      })
+          .then(response => {
+            this.models = response.data
+            console.log(response.data)
+          }).catch(error => {
+        console.log(error)
+      })
     }
   },
   beforeMount() {
     this.showBikeTypes()
   }
 }
-
 
 
 </script>
